503eecae2920ada299a1ea71a29712820ba0fb83
yehaoze <yehaozee@gmail.com>
2025-03-22 22:03:37 +0100

    kezdolap telefon

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,45 +15,45 @@ export class HomeComponent implements AfterViewInit {
   @ViewChildren('child') children!: QueryList<ElementRef>;
 
   ngAfterViewInit() {
+    const isMobile = window.matchMedia('(max-width: 768px)').matches;
     const observerOptions = {
       root: null,
       rootMargin: '0px',
-      threshold: 0.6
+      threshold: isMobile ? 0.4 : 0.6 
     };
-
+  
     const observer = new IntersectionObserver((entries, observer) => {
       entries.forEach((entry, index) => {
         if (entry.isIntersecting) {
           const element = entry.target as HTMLElement;
-          
-          
+  
           setTimeout(() => {
             if (element.classList.contains('divcontainer_III_text')) {
               element.classList.add('moveLeft');
-            }
-            else if (element.classList.contains('divcontainer') && element.classList.contains('II')) {
+            } else if (element.classList.contains('divcontainer') && element.classList.contains('II')) {
               element.classList.add('fadeInFromLeft');
-            }
-            else if (element.classList.contains('shop')) {
+            } else if (element.classList.contains('shop')) {
               element.classList.add('fadeInAndGrow');
-            }
-            else if ( element.classList.contains('good1') || element.classList.contains('good2') || element.classList.contains('good3') ) {
+            } else if (
+              element.classList.contains('good1') ||
+              element.classList.contains('good2') ||
+              element.classList.contains('good3')
+            ) {
               element.classList.add('slide-up2');
-            }
-            else {
+            } else {
               element.classList.add('slide-up');
             }
           }, index * 400);
   
-          observer.unobserve(entry.target); 
+          observer.unobserve(entry.target);
         }
       });
     }, observerOptions);
   
-    this.children.forEach(child => {
-      observer.observe(child.nativeElement); 
+    this.children.forEach((child) => {
+      observer.observe(child.nativeElement);
     });
-  }
+  }  
   
 
   ngOnInit() {
